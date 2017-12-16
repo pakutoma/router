dfce9b2ce1c3165efe5d67e43c1cd3cb95aed161
Chiba Tairi <tairi0717@gmail.com>
2017-12-17 02:39:18 +0900

    使っていないものは消す

--- checksum.c
+++ checksum.c
@@ -22,11 +22,3 @@ unsigned short calc_checksum(uint8_t *header, int32_t length) {
 
     return ~sum;
 }
-
-uint32_t calc_fcs(uint8_t *data, int32_t length) {
-    uint32_t crc = 0xffffffff;
-    for (; length--; data++) {
-        crc = ((crc >> 8) & 0x00FFFFFF) ^ crc32_table[(crc ^ (*data)) & 0xFF];
-    }
-    return ~crc;
-}
--- checksum.h
+++ checksum.h
@@ -1,4 +1,3 @@
 #pragma once
 #include <stdint.h>
 uint16_t calc_checksum(uint8_t *header, int32_t length);
-uint32_t calc_fcs(uint8_t *data, int32_t length);
