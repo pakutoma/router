b24e3078a0e3a464852e02ba7700af757aba3b25
Chiba Tairi <tairi0717@gmail.com>
2018-01-21 17:21:37 +0900

    一旦

--- arp_waiting_list.c
+++ arp_waiting_list.c
@@ -3,6 +3,7 @@
 #include "send_queue.h"
 #include <net/ethernet.h>
 #include <netinet/ip.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- arp_waiting_list.h
+++ arp_waiting_list.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "ether_frame.h"
 #include <net/ethernet.h>
+#include <stdint.h>
 int init_arp_waiting_list();
 int add_arp_waiting_list(ether_frame_t *ether_frame, uint32_t neighbor_ipaddr);
 void send_waiting_ethernet_frame_matching_ipaddr(uint32_t ipaddr, uint8_t macaddr[ETH_ALEN]);
